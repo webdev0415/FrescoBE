8ddd27f5335f9f1187c586ec3a79aff33fc1beb5
umer <umer@digidiv.pk>
2020-11-13 04:00:24 +0500

    resend verification email for signup created

---
+++ src/modules/auth/dto/ResendConfirmationEmail.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+import { ApiProperty } from '@nestjs/swagger';
+import {IsEmail, IsNotEmpty, IsString} from "class-validator";
+import {Column} from "typeorm";
+
+export class ResendConfirmationEmail {
+    @IsString()
+    @IsEmail()
+    @IsNotEmpty()
+    @ApiProperty()
+    @Column()
+    email: string;
+
+
+}
