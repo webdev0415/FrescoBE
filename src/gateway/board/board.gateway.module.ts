2ff7498a1584b81b3450cd1a1271dd1b3336b915
Hovhannes Zhamharyan <hovhannes.zhamharyan@gmail.com>
2020-12-09 19:08:16 +0400

    check socket connection

--- src/gateway/board/board.gateway.module.ts
+++ src/gateway/board/board.gateway.module.ts
@@ -1,11 +1,19 @@
-import {forwardRef, Module} from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import {UserModule} from '../../modules/user/user.module';
-// import { ConfigService } from '../shared/services/config.service';
-import {BoardGateway} from './board.gateway';
+import { UserToOrgRepository } from '../../modules/user-org/user-org.repository';
+import { UserModule } from '../../modules/user/user.module';
+import { UserRepository } from '../../modules/user/user.repository';
+import { UserService } from '../../modules/user/user.service';
+import { ConfigService } from '../../shared/services/config.service';
+import { BoardGateway } from './board.gateway';
 
 @Module({
-    imports: [forwardRef(() => UserModule)],
-    providers: [BoardGateway],
+    imports: [
+        TypeOrmModule.forFeature([UserRepository, UserToOrgRepository]),
+        forwardRef(() => UserModule),
+    ],
+    providers: [BoardGateway, UserService, ConfigService],
+    exports: [UserService, ConfigService],
 })
 export class BoardGatewayModule {}
--- src/modules/message/message.module.ts
+++ src/modules/message/message.module.ts
@@ -3,16 +3,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { BoardGateway } from '../../gateway/board/board.gateway';
 import { BoardUserOrgRepository } from '../board-user-org/board-user-org.repository';
+import { UserToOrgRepository } from '../user-org/user-org.repository';
+import { UserRepository } from '../user/user.repository';
+import { UserService } from '../user/user.service';
 import { MessageController } from './message.controller';
 import { MessageRepository } from './message.repository';
 import { MessageService } from './message.service';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([MessageRepository, BoardUserOrgRepository]),
+        TypeOrmModule.forFeature([
+            MessageRepository,
+            BoardUserOrgRepository,
+            UserToOrgRepository,
+            UserRepository,
+        ]),
     ],
     controllers: [MessageController],
-    providers: [MessageService, BoardGateway],
-    exports: [MessageService, BoardGateway],
+    providers: [MessageService, BoardGateway, UserService],
+    exports: [MessageService, BoardGateway, UserService],
 })
 export class MessageModule {}
