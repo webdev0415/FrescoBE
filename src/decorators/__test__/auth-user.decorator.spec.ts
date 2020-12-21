1dfdcd1ee0f5a23b8104b11d86b02c92080e7196
igorshevk <igor.shevk@gmail.com>
2020-12-21 23:37:16 +0330

    Define unit tests for AuthUser and Roles decorators and WsExceptionFilter

--- src/decorators/__test__/auth-user.decorator.spec.ts
+++ src/decorators/__test__/auth-user.decorator.spec.ts
@@ -1,10 +1,52 @@
-import { AuthUser } from '../auth-user.decorator';
+import { mockExecutionContext } from '../../__test__/nestjs-helper-mock.spec';
 
 describe('AuthUser', () => {
-    it('AuthUser snapshot ', () => {
+    beforeEach(() => {
+        jest.resetModules();
+        jest.resetModuleRegistry();
+    });
+
+    it('AuthUser snapshot ', async () => {
+        const { AuthUser } = await import('../auth-user.decorator');
         expect(AuthUser).toMatchSnapshot();
     });
-    it('AuthUser to be called ', () => {
-        expect(AuthUser({})).toBeTruthy()
+
+    it('AuthUser to be called ', async () => {
+        const { AuthUser } = await import('../auth-user.decorator');
+        expect(AuthUser({})).toBeTruthy();
+    });
+
+    it('should pass user as argument', async () => {
+        const user = {
+            id: 1,
+            name: 'john',
+        };
+
+        // Mock request
+        const executionContext = mockExecutionContext;
+        const getRequest = executionContext.switchToHttp()
+            .getRequest as jest.Mock;
+        getRequest.mockImplementation(() => ({
+            user,
+        }));
+
+        const mockCreateParamDecorator = (factory) => {
+            return () => {
+                const result = factory(null, executionContext);
+                expect(result).toBe(user);
+            };
+        };
+
+        jest.mock('@nestjs/common', () => ({
+            ...jest.requireActual('@nestjs/common'),
+            createParamDecorator: mockCreateParamDecorator,
+        }));
+
+        // Test
+        const { AuthUser } = await import('../auth-user.decorator');
+        AuthUser();
+
+        // Assert
+        expect(getRequest.mock.calls.length).toBe(1);
     });
 });
--- src/decorators/__test__/roles.decorator.spec.ts
+++ src/decorators/__test__/roles.decorator.spec.ts
@@ -1,7 +1,31 @@
-import { Roles } from '../roles.decorator';
+import { RoleType } from '../../common/constants/role-type';
 
 describe('Roles', () => {
-    it('Roles snapshot ', () => {
+    beforeEach(() => {
+        jest.resetModules();
+    });
+
+    it('Roles snapshot ', async () => {
+        const { Roles } = await import('../roles.decorator');
         expect(Roles).toMatchSnapshot();
+    });
+
+    it('should call SetMetadata with roles', async () => {
+        const roles = [RoleType.ADMIN];
+
+        const mockSetMetadata = jest.fn((key, value) => {
+            expect(key).toBe('roles');
+            expect(value).toEqual(roles);
+        });
+
+        jest.mock('@nestjs/common', () => ({
+            ...jest.requireActual('@nestjs/common'),
+            SetMetadata: mockSetMetadata,
+        }));
+
+        const { Roles } = await import('../roles.decorator');
+        Roles(...roles);
+
+        expect(mockSetMetadata.mock.calls.length).toBe(1);
     });
 });
